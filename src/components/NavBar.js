d7b42c7a75b9dc58c8b70319205d09f934bdf3ab
Nick Boober <nboober@terpmail.umd.edu>
2020-03-17 16:39:05 -0400

    implemented a working navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 class NavBar extends React.Component{
     render(){
         return(
-            <div>navbar</div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/users">Users</Link>
+                    </li>
+                </ul>
+            </div>
+            </nav>
         )
     }
 }
